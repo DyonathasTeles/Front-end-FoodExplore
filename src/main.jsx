09870b4ff32fb8fce9c65009c49e3f688afe5f79
DyonathasTeles <telesdyonathas@gmail.com>
2023-07-25 15:19:39 -0300

    global settings

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
+import GlobalStyle from './style/GlobalStyle'
 import theme from "./style/theme"
 
 import { SignIn } from './pages/SignIn'
@@ -8,6 +9,7 @@ import { SignIn } from './pages/SignIn'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+    <GlobalStyle/>
     <SignIn />
     </ThemeProvider>
   </React.StrictMode>,
---
+++ src/style/GlobalStyle.js
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  *{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  :root {
+
+    font-size: 62.5%;
+  }
+
+  body {
+    font-size: 1.6rem;
+    background-color: ${({ theme }) => theme.COLORS.DARK_400};
+    color: ${({ theme }) => theme.COLORS.LIGHT_300};
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  button, a {
+    cursor: pointer;
+    transition: filter 200ms;
+  }
+
+  button:hover, a:hover {
+    filter: brightness(0.9);
+  }
+
+`
